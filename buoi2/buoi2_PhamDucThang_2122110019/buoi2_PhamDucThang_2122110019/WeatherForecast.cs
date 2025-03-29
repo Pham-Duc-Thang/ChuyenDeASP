c60439f0dd843ad8d2bd7812d716697b305a797e
Pham-Duc-Thang <145883653+Pham-Duc-Thang@users.noreply.github.com>
2025-03-29 13:31:24 +0700

    First commit

---
+++ buoi2/buoi2_PhamDucThang_2122110019/buoi2_PhamDucThang_2122110019/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace buoi2_PhamDucThang_2122110019
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
